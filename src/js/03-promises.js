182d5ba638fb632bb6fa0f78e4c6f9d1955ef375
Katya-Semeniuk <katyasemeniuk@gmail.com>
2022-12-18 14:05:29 +0100

    to mentor

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -33,80 +33,15 @@ function initPromiseCreation(e) {
 
   let currentAmount = 0;
   let currentDelay = +delay;
+  
 
   setTimeout(() => {
     const interval = setInterval(() => {
       currentAmount++;
       createPromise(currentAmount, currentDelay);
-      currentDelay += +step;
+      currentDelay  += +step;
 
       if (currentAmount === +amount) clearInterval(interval);
-    }, +step);
+    }, +step );
   }, +delay);
 }
-
-
-
-
-
-
-// const form = document.querySelector('.form');
-// const formDelay = document.getElementsByName("delay");
-// const formStep = document.getElementsByName("step");
-// const formamount = document.getElementsByName("amount");
-
-
-// form.addEventListener('submit', onFormSubmit);
-
-// let delay = formDelay.value;
-// let step = formStep.value;
-// let amount = formamount.value;
-
-
-// function onFormSubmit(e) {
-//    e.preventDefault();
-
-//   let currentAmount = 0;
-//   let currentDelay = + delay;
-   
-//   setTimeout(() => {
-    
-//     const interval = setInterval(() => {
-//       currentAmount++;
-//       createPromise(currentAmount, currentDelay);
-//       currentDelay += +step;
-
-//       if (currentAmount === amount) {
-//       clearInterval(interval)
-//     }
-//     }, step)
-     
-//   }, delay)
-// }
-
-
-
-// function createPromise(position, delay) {
-
-//  return new Promise((resolve, reject) => {
-//   const shouldResolve = Math.random() > 0.3;
-  
-//     if (shouldResolve) {
-//       resolve({ position, delay })
-//       console.log("Проміс виконався успішно")
-
-//     } else {
-//       reject({ position, delay })
-//       console.log("Проміс виконався з помилкою")
-//     }
-//   }
-//   )
-// }
-
-// createPromise()
-//   .then(({ position, delay }) => {
-//     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
